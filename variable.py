e324fbedb2424db5628ccf87d8225111339a9d06
prishaj05-rgb <prishaj05@gmail.com>
2026-01-14 16:17:25 +1100

    variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x=5
+y="John"
+print(x)
+print(y)
+
+codingal="Codingal"
+print(codingal)
+
+#User input
+name = input("enter your name: ")
+
+print("Hello",name,"\nwelcome to codingal")
