c858aba5f3c2fc4bceafa783409649cb320920da
prishaj05-rgb <prishaj05@gmail.com>
2026-01-14 16:08:25 +1100

    print

---
+++ activity_1.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of programming")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of print statement
+print("welcome to", end= "*")
