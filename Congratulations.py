a332a23cef736010da4adcb432b4aa2c44353647
prishaj05-rgb <prishaj05@gmail.com>
2026-01-21 16:44:43 +1100

    congratulations homework

---
+++ Congratulations.py
@@ -0,0 +1,17 @@
+#Convert a string into Upper Case
+x = "CoNgRaTuLAtionS"
+print(str.lower(x))
+
+print(str.upper(x))
+
+#Take two strings as input from user
+print("Enter the first string: ")
+strOne = input()
+
+print("Enter the second string: ")
+strTwo = input()
+
+#Join both strings
+#Using additional opperator
+strThree = strOne + " " + strTwo
+print("\nJoined strings: " , strThree)
