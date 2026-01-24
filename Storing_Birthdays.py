be8412f90211cdd96f50001f36d48a9ece8a2abd
prishaj05-rgb <prishaj05@gmail.com>
2026-01-24 18:53:54 +1100

    Storing Birthdays

---
+++ Storing_Birthdays.py
@@ -0,0 +1,12 @@
+print("Birthday of 5 friends\n")
+
+a = "31/10/08"
+print("Shreya - " , a)
+b = "11/02/13"
+print("Arya - " , b)
+c = "21/04/11"
+print("Saina - " , c)
+d = "23/04/09"
+print("Tanya -" , d)
+e = "05/09/11"
+print("Prisha - " , e)
