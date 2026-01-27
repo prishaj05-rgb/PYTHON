45aaba2bd45a210611ea7e4b6fc60db324f8aad3
prishaj05-rgb <prishaj05@gmail.com>
2026-01-27 16:07:11 +1100

    finding average

---
+++ activity_3_Average.py
@@ -0,0 +1,14 @@
+#Storing Values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#Finding total of trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The sum of all the 5 trees is:" , sum)
+
+#Finding average of all trees
+average = sum/5
+print("The average of all trees is:" , average)
