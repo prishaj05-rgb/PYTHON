3a5ffb79fec84cff787785e12276667dd95d099a
prishaj05-rgb <prishaj05@gmail.com>
2026-02-04 16:31:50 +1100

    squareroot HW

--- Squareroot.py
+++ Squareroot.py
@@ -2,3 +2,8 @@ num = 49
 num_sqrt = num ** 0.5
 print(f'The number is {num} and it is stored in num_sqrt {num_sqrt}')
 #python Program to calculate the square root
+
+num = int(input("enter number:"))
+
+num_sqrt = num**0.5
+print('the square root of %0.2f is %0.3f' % (num, num_sqrt))
