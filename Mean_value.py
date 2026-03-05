c9bdd3f3239e31a06a2da8b6f650ad6b386f1195
prishaj05-rgb <prishaj05@gmail.com>
2026-03-05 19:55:39 +1100

    mean value

---
+++ Mean_value.py
@@ -0,0 +1,19 @@
+mean1 = 38
+wrong_number = 36
+correct_number=56
+total_num=40
+
+#sum of 40 numbers
+
+sum = mean1 * total_num
+print("the sum of 40 numebrs is" ,sum)
+
+#correct sum
+
+num2=sum - ((wrong_number) - (correct_number))
+print("sum - ((wrong_number) - (correct_number)) " ,num2)
+
+#the correct mean 
+
+mean2 = num2/total_num
+print(mean2)
