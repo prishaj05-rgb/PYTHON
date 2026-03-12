b726dd8c815ee5011017f591e9a3cdfea6aa9728
prishaj05-rgb <prishaj05@gmail.com>
2026-03-12 20:55:56 +1100

    ww

---
+++ custom_ride.py
@@ -0,0 +1,16 @@
+print("select your ride")
+print("1. Bike")
+print("2. Car")
+
+#take input of number 1 and 2
+#select your ride
+choice = int(input("Enter your choice: "))
+
+#user entering option 1
+if (choice == 1): #condition 1 outer if statement
+    print("what type of bike")
+    print("1. scooter\n")
+    print("2. scooty\n")
+
+    #condition for selecting the type of bike
+    choice2=int(input("Enter your choice 2: "))
