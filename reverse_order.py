5487a6ae9c23ad83ee006279ee9d3030b435dd11
prishaj05-rgb <prishaj05@gmail.com>
2026-03-19 19:56:58 +1100

    reverse order of numbers 19.3.26

---
+++ reverse_order.py
@@ -0,0 +1,12 @@
+# input number greater than 1
+
+n = int(input("Enter the value of n:"))
+
+#print numbers 1 to n
+
+print ("numbers from {0} to {1} are : ".format (n,1))
+
+#loop to print numbers
+
+for i in range (n,0,-1):
+    print(i)
