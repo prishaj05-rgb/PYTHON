cc47beb10f663dce6310c031dcafff92c8686db7
prishaj05-rgb <prishaj05@gmail.com>
2026-03-19 19:28:09 +1100

    age limit

---
+++ check_age.py
@@ -0,0 +1,7 @@
+age = int(input("how old are you?:"))
+
+if age > 10 and age <20:
+    print("you are allowed in a class age limit 10 to 20")
+
+elif age < 10 or age >20:
+    print("you are not allowed in class")
