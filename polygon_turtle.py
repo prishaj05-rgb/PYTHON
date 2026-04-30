92a05ecde4504d5fd575b88a0a54eb8016025b04
prishaj05-rgb <prishaj05@gmail.com>
2026-04-30 18:44:59 +1000

    making polygon with turtle

---
+++ polygon_turtle.py
@@ -0,0 +1,17 @@
+import turtle #import library
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+
+polygon = turtle.Turtle() #defined variable 
+
+num_sides = 6 #variable
+side = 70
+angle = 360.0 / num_sides
+
+#iterate loop for total number of sides
+
+for i in range(num_sides):
+    polygon.forward(side)
+    polygon.right(angle)
+
+turtle.done()
