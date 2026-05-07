33dd0d739a26175c465e671df8547eb755f39b18
prishaj05-rgb <prishaj05@gmail.com>
2026-05-07 15:11:30 +1000

    hw

---
+++ prisha.turtle.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.Screen().bgcolor("purple")
+turtle.Screen().setup(300,400)
+name = turtle.Turtle()
+
+name.right(90)
+name.forward(100)
+name.penup()
+name.left(180)
+name.forward(100)
+name.pendown()
+name.right(90)
+name.forward(50)
+name.right(90)
+name.forward(50)
+name.right(90)
+name.forward(50)
+
+turtle.done()
