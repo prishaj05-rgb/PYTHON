97cb8b59a8e3301ee040c77d80c1ee082f1bc770
prishaj05-rgb <prishaj05@gmail.com>
2026-05-09 12:54:19 +1000

    how much to tip the waiter positional argument 9.5.26

---
+++ tip_the_waiter.py
@@ -0,0 +1,9 @@
+def total_calc(bill_amount, tip_perc):
+    total = bill_amount*(1+0.01*tip_perc)
+    total = round(total,2)
+    print(f"please pay ${total}")
+
+#specify only bill_amount
+#default value of tip percentage is used
+
+total_calc(150,20)
