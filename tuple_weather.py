ac38f330eb09ecd8bea2a674f03c33b2898fc07c
prishaj05-rgb <prishaj05@gmail.com>
2026-07-09 16:04:03 +1000

    tuple weather 09.07.26

---
+++ tuple_weather.py
@@ -0,0 +1,14 @@
+weather = (1,0,0,0,1,1,0)
+sunny = 0
+rainy = 0
+
+for i in range(0,7):
+    if weather[i] == 1:
+        sunny += 1
+    else:
+        rainy += 1
+
+if (sunny>rainy):
+    print("it is sunny outside")
+else:
+    print("it is rainy outside")
