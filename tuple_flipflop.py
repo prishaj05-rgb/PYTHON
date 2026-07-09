760188a5a742928a570858d0cfe3398593409c6c
prishaj05-rgb <prishaj05@gmail.com>
2026-07-09 15:52:26 +1000

    flip flop tuple 09.07.26

---
+++ tuple_flipflop.py
@@ -0,0 +1,17 @@
+# function to check whether palidrome or not
+def palind(r):
+    e = len(r) -1
+    s = 0
+    while(s<e):
+        if r[s] != r[e]:
+            return False
+        s += 1
+        e -= 1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if (palind(r)):
+    print("the given tuple is a flip flop")
+else:
+    print("the given tuple is not a flip flop")
